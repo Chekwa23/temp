d52d04375ca6d2c9754a378fbd05fbf250d6a59f
kavs g <g.kavs009@gmail.com>
2021-01-04 18:48:43 +0000

    added candidate.js and company.js file

---
+++ candidateSignUp.js
@@ -0,0 +1,43 @@
+
+document.getElementById("SignUp").onclick = function () {
+    const firstname = document.querySelector('#exampleInputFirstName').value;
+    const lastname = document.querySelector('#exampleInputLastName').value;
+    const email = document.querySelector('#exampleInputEmail1').value;
+    const password = document.querySelector('#exampleInputPassword1').value;
+    const confirmpassword = document.querySelector('#exampleInputPassword2').value;
+    if (firstname != "" && lastname != "" && email != "" && password != "" && confirmpassword != "") {
+
+        if (confirmpassword != password) {
+            window.alert("Passwords donot match");
+        } else {
+            var firebaseConfig = {
+                apiKey: "AIzaSyDwjbCyTjacYVq8GaJP1wSX9YiBbA0OWqA",
+                authDomain: "seecking-ca8a8.firebaseapp.com",
+                projectId: "seecking-ca8a8",
+                storageBucket: "seecking-ca8a8.appspot.com",
+                messagingSenderId: "692349145986",
+                appId: "1:692349145986:web:f684e8acfd0620b0a7b59b",
+                measurementId: "G-JB4362BWEB"
+            };
+            firebase.initializeApp(firebaseConfig);
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+                .then((user) => {
+                   location.href = "candidateForm.html";
+                })
+                .catch((error) => {
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    console.log(errorCode);
+                    window.alert(errorMessage);
+                });
+           
+        }
+    } else {
+        window.alert("Form is incomplete,please fill all the fields");
+    }
+
+}
+
+
+
+     
